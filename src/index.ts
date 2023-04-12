fa043d59f77567a23c25da653d0b63ae872a7ee9
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2023-04-12 14:22:29 -0400

    feat: setup typegoose

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import * as mongoose from "mongoose";
+import get from "lodash.get";
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
 const typeDefs = `#graphql
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
@@ -20,6 +19,7 @@ const typeDefs = `#graphql
     books: [Book]
   }
 `;
+
 const books = [
   {
     title: "The Awakening",
@@ -29,26 +29,28 @@ const books = [
     title: "City of Glass",
     author: "Paul Auster",
   },
-]; // Resolvers define how to fetch the types defined in your schema.
-// This resolver retrieves books from the "books" array above.
+];
+
 const resolvers = {
   Query: {
     books: () => books,
   },
-}; // The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
+};
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-});
-
-console.log(`🚀  Server ready at: ${url}`);
-
+mongoose
+  .connect(`mongodb://localhost:27017/`, { dbName: "guides" })
+  .then(
+    (): Promise<{ url: string }> =>
+      startStandaloneServer(server, { listen: { port: 4000 } })
+  )
+  .then(({ url }: { url: string }) =>
+    console.log(`🚀  Server ready at: ${url}`)
+  )
+  .catch((err: Error) => {
+    console.error(get(err, "message", "Something went wrong."));
+  });
