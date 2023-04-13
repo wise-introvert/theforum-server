f96157f0f71d63c5aa92ee172f948600031dc2ff
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2023-04-13 04:48:33 -0400

    refactor: imports

--- src/forum/index.ts
+++ src/forum/index.ts
@@ -1,3 +1,4 @@
 export * from "./dto";
 export * from "./model";
 export * from "./resolver";
+export * from "./service";
--- src/post/index.ts
+++ src/post/index.ts
@@ -1,3 +1,4 @@
+export * from "./service";
 export * from "./dto";
 export * from "./model";
 export * from "./resolver";
