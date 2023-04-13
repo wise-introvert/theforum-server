dff220f49f841786cd48ce855546c6e19685b3bd
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2023-04-13 04:45:51 -0400

    refactor: migrate mutations/queries to service

--- src/forum/resolver.ts
+++ src/forum/resolver.ts
@@ -1,63 +1,17 @@
 import { Mutation, Resolver, Arg, Query } from "type-graphql";
-import { Types } from "mongoose";
-import get from "lodash.get";
-import isEmpty from "lodash.isempty";
-import md5 from "md5";
 
-import {
-  Forum,
-  createForumValidationSchema,
-  CreateForumInput,
-  ForumModel,
-} from ".";
-import { User, UserModel } from "../user";
+import { Forum, CreateForumInput } from ".";
+import * as ForumService from "./service";
 
 @Resolver(Forum)
 export class ForumResolver {
   @Query(() => [Forum])
   async forums(): Promise<Forum[]> {
-    return ForumModel.find();
+    return ForumService.forums();
   }
 
   @Mutation(() => Forum)
   async forum(@Arg("input") input: CreateForumInput): Promise<Forum> {
-    try {
-      // validate input
-      await createForumValidationSchema.validate(input);
-
-      const author: User | null = await UserModel.findOne({
-        _id: new Types.ObjectId(get(input, "author", "")),
-      });
-
-      if (isEmpty(author)) {
-        throw new Error("Invalid author!");
-      }
-
-      const forum: Forum = await ForumModel.create({
-        ...input,
-        image: get(
-          input,
-          "image",
-          `https://www.gravatar.com/avatar/${md5(
-            `${get(input, "title", "")}@forum.com`
-          )}?s=500&d=identicon`
-        ),
-        author,
-      });
-
-      author.forums?.push(forum._id);
-
-      await UserModel.updateOne({ _id: author._id }, author);
-
-      return forum;
-    } catch (err) {
-      throw new Error(
-        get(
-          err,
-          "message",
-          "Something went wrong. Please try again after some time."
-        )
-      );
-    }
+    return ForumService.forum(input);
   }
 }
---
+++ src/forum/service.ts
@@ -0,0 +1,55 @@
+import { Types } from "mongoose";
+import get from "lodash.get";
+import isEmpty from "lodash.isempty";
+import md5 from "md5";
+
+import {
+  Forum,
+  createForumValidationSchema,
+  CreateForumInput,
+  ForumModel,
+} from ".";
+import { User, UserModel } from "../user";
+
+export const forums = async () => ForumModel.find();
+
+export const forum = async (input: CreateForumInput): Promise<Forum> => {
+  try {
+    // validate input
+    await createForumValidationSchema.validate(input);
+
+    const author: User | null = await UserModel.findOne({
+      _id: new Types.ObjectId(get(input, "author", "")),
+    });
+
+    if (isEmpty(author)) {
+      throw new Error("Invalid author!");
+    }
+
+    const forum: Forum = await ForumModel.create({
+      ...input,
+      image: get(
+        input,
+        "image",
+        `https://www.gravatar.com/avatar/${md5(
+          `${get(input, "title", "")}@forum.com`
+        )}?s=500&d=identicon`
+      ),
+      author,
+    });
+
+    author.forums?.push(forum._id);
+
+    await UserModel.updateOne({ _id: author._id }, author);
+
+    return forum;
+  } catch (err) {
+    throw new Error(
+      get(
+        err,
+        "message",
+        "Something went wrong. Please try again after some time."
+      )
+    );
+  }
+};
